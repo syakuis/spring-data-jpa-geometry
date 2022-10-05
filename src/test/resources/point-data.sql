c0816b3ddf57fd4a9498039d75713c2bc34dabf9
syakuis <syaku@naver.com>
2022-10-06 03:53:54 +0900

    작업

--- src/test/resources/point-data.sql
+++ src/test/resources/point-data.sql
@@ -1,8 +1,8 @@
-insert into geo_point (id, name, latitude, longitude, point) values (1, '파크원', 37.526667, 126.927141, ST_GeomFromText('POINT 37.526667 126.927141'));
-insert into geo_point (id, name, latitude, longitude, point) values (2, 'IFC', 37.525243, 126.925694, ST_GeomFromText('POINT 37.525243 126.925694'));
-insert into geo_point (id, name, latitude, longitude, point) values (3, '롯데타워', 37.512603, 127.102228, ST_GeomFromText('POINT 37.512603 127.102228'));
-insert into geo_point (id, name, latitude, longitude, point) values (4, '올림픽공원역 1번출구 앞 따릉이 대여소', 37.516730, 127.131305, ST_GeomFromText('POINT 37.516730 127.131305'));
-insert into geo_point (id, name, latitude, longitude, point) values (5, '올림픽대교남단', 37.525619, 127.117681, ST_GeomFromText('POINT 37.525619 127.117681'));
-insert into geo_point (id, name, latitude, longitude, point) values (6, 'NH투자증권 본사', 37.526964, 126.927675, ST_GeomFromText('POINT 37.526964 126.927675'));
-insert into geo_point (id, name, latitude, longitude, point) values (7, '여의도동', 37.526702, 126.926791, ST_GeomFromText('POINT 37.526702 126.926791'));
-insert into geo_point (id, name, latitude, longitude, point) values (8, '뱅크샐러드', 37.526681, 126.926968, ST_GeomFromText('POINT 37.526681 126.926968'));
+insert into geo_point (id, name, latitude, longitude, point) values (1, '파크원', 37.526667, 126.927141, ST_GeomFromText('POINT 126.927141 37.526667'));
+insert into geo_point (id, name, latitude, longitude, point) values (2, 'IFC', 37.525243, 126.925694, ST_GeomFromText('POINT 126.925694 37.525243'));
+insert into geo_point (id, name, latitude, longitude, point) values (3, '롯데타워', 37.512603, 127.102228, ST_GeomFromText('POINT 127.102228 37.512603'));
+insert into geo_point (id, name, latitude, longitude, point) values (4, '올림픽공원역 1번출구 앞 따릉이 대여소', 37.516730, 127.131305, ST_GeomFromText('POINT 127.131305 37.516730'));
+insert into geo_point (id, name, latitude, longitude, point) values (5, '올림픽대교남단', 37.525619, 127.117681, ST_GeomFromText('POINT 127.117681 37.525619'));
+insert into geo_point (id, name, latitude, longitude, point) values (6, 'NH투자증권 본사', 37.526964, 126.927675, ST_GeomFromText('POINT 126.927675 37.526964'));
+insert into geo_point (id, name, latitude, longitude, point) values (7, '여의도동', 37.526702, 126.926791, ST_GeomFromText('POINT 126.926791 37.526702'));
+insert into geo_point (id, name, latitude, longitude, point) values (8, '뱅크샐러드', 37.526681, 126.926968, ST_GeomFromText('POINT 126.926968 37.526681'));
--- src/test/resources/table.sql
+++ src/test/resources/table.sql
@@ -6,3 +6,22 @@ create table location (
                           point GEOMETRY not null,
                           primary key (id)
 ) engine=InnoDB
+
+-- SELECT r.*, ST_Distance_Sphere(r.point, ST_GeomFromText('POINT(37.513982 127.101581)')) as distance
+-- FROM test AS r
+-- WHERE ST_Distance_Sphere(r.point, ST_GeomFromText('POINT(37.513982 127.101581)')) <= 2000;
+--
+-- SELECT r.*, ST_Distance_Sphere(r.point, ST_GeomFromText('POINT(127.101581 37.513982)')) as distance
+-- FROM test AS r
+-- WHERE ST_Distance_Sphere(r.point, ST_GeomFromText('POINT(127.101581 37.513982)')) <= 2000;
+--
+-- SELECT r.*, ST_Distance(r.point, ST_GeomFromText('POINT(127.101581 37.513982)')) as distance
+-- FROM test AS r;
+--
+-- SELECT r.*
+-- FROM test AS r
+-- where ST_WITHIN(r.point, ST_GEOMFROMTEXT('POLYGON((126.926788 37.536702, 126.927184 37.526935, 126.927502 37.526634, 126.927106 37.526392, 126.926788 37.536702))'));
+--
+-- SELECT r.*
+-- FROM test AS r
+-- where MBRContains(ST_LINESTRINGFROMTEXT('LINESTRING(126.926774 37.526700, 126.927507 37.526626)'), r.point);
